761e144cbfb28f0fc2d4752e861f7abdf33a504e
YuriyMitrofanov <yurasell@yandex.ru>
2024-03-03 22:24:55 +0300

    work copy

--- src/app/pages/MainPage/MainPage.jsx
+++ src/app/pages/MainPage/MainPage.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import images from "../../assets/images/images.json";
-import { FreeMode, Mousewheel } from "swiper/modules";
+import { FreeMode, Mousewheel, Parallax } from "swiper/modules";
+// import { CenteredSlides } from "swiper/types/modules/swiper-options.d.ts.365";
 
 const MainPage = () => {
-    console.log(images);
     return (
         <div className="swiper slider slider-main">
             <Swiper
-                className="swioer-wrapper slider__wrapper"
-                modules={[FreeMode, Mousewheel]}
+                className="swiper-wrapper slider__wrapper"
+                modules={[FreeMode, Mousewheel, Parallax]}
                 freeMode={true}
-                mousewheel
+                mousewheel={true}
                 breakpoints={{
                     0: {
                         slidesPerView: 2.5,
@@ -23,18 +23,21 @@ const MainPage = () => {
                         spaceBetween: 60
                     }
                 }}
-                // spaceBetween={50}
-                // slidesPerView={2.5}
+                parallax={true}
+                // centeredSlides={true}
                 onSlideChange={() => console.log("slide change")}
                 onSwiper={(swiper) => console.log(swiper)}
             >
                 {images.map(item => (
                     <SwiperSlide 
                         key={item.id}
-                        className="swioer-slide swiper__item"
+                        className="swiper-slide slider__item"
                     >
-                        {/* <img src={item.img} alt={item.title} /> */}
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro laboriosam aspernatur corporis sapiente necessitatibus et ipsum nulla minus officia quo mollitia commodi perspiciatis quaerat, quam autem. Eaque molestias necessitatibus sequi.
+                        <div
+                            className="slider__img"
+                            data-swiper-parallax="20%"
+                            style={{backgroundImage: `url("${item.img}")`}}
+                        ></div>
                     </SwiperSlide>
                 ))}
             </Swiper>
