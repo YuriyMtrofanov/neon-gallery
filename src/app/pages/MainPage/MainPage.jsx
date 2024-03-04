ceae03c0ebcf499d9992f05d2536b1bed766f7e2
YuriyMitrofanov <yurasell@yandex.ru>
2024-03-04 22:41:03 +0300

    combined two sliders

--- src/app/pages/MainPage/MainPage.jsx
+++ src/app/pages/MainPage/MainPage.jsx
@@ -1,47 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import images from "../../assets/images/images.json";
-import { FreeMode, Mousewheel, Parallax } from "swiper/modules";
-// import { CenteredSlides } from "swiper/types/modules/swiper-options.d.ts.365";
+import { FreeMode, Mousewheel, Parallax, Controller } from "swiper/modules";
 
 const MainPage = () => {
+    const [firstSwiper, setFirstSwiper] = useState(null);
+    const [secondSwiper, setSecondSwiper] = useState(null);
+
     return (
-        <div className="swiper slider slider-main">
-            <Swiper
-                className="swiper-wrapper slider__wrapper"
-                modules={[FreeMode, Mousewheel, Parallax]}
-                freeMode={true}
-                mousewheel={true}
-                breakpoints={{
-                    0: {
-                        slidesPerView: 2.5,
-                        spaceBetween: 20
-                    },
-                    680: {
-                        slidesPerView: 3.5,
-                        spaceBetween: 60
-                    }
-                }}
-                parallax={true}
-                // centeredSlides={true}
-                onSlideChange={() => console.log("slide change")}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                {images.map(item => (
-                    <SwiperSlide 
-                        key={item.id}
-                        className="swiper-slide slider__item"
-                    >
-                        <div
-                            className="slider__img"
-                            data-swiper-parallax="20%"
-                            style={{backgroundImage: `url("${item.img}")`}}
-                        ></div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+        <>
+            <div className="swiper slider slider-main">
+                <Swiper
+                    className="swiper-wrapper slider__wrapper"
+                    modules={[FreeMode, Mousewheel, Parallax, Controller]}
+                    freeMode={true}
+                    mousewheel={true}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 2.5,
+                            spaceBetween: 20
+                        },
+                        680: {
+                            slidesPerView: 3.5,
+                            spaceBetween: 60
+                        }
+                    }}
+                    parallax={true}
+                    centeredSlides={true}
+                    onSwiper={setFirstSwiper}
+                    controller={{ control: secondSwiper }}
+                >
+                    {images.map(item => (
+                        <SwiperSlide 
+                            key={item.id}
+                            className={"swiper-slide slider__item"}
+                            // className={() => handleChangeClassname()}
+                            // onClick={() => handleClic(item.id)}
+                        >
+                            <div
+                                className="slider__img"
+                                data-swiper-parallax={item.id % 2 ? "20%" : "30%"}
+                                style={{backgroundImage: `url("${item.img}")`}}
+                            ></div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="slider-background">
+                <Swiper
+                    className="swiper-wrapper slider__wrapper"
+                    modules={[FreeMode, Mousewheel, Parallax, Controller]}
+                    freeMode={true}
+                    centeredSlides={true}
+                    parallax={true}
+                    slidesPerView={3.5}
+                    spaceBetween={60}
+                    onSwiper={setSecondSwiper}
+                    controller={{ control: firstSwiper }}
+                >
+                    {images.map(item => (
+                        <SwiperSlide 
+                            key={item.id}
+                            className="swiper-slide slider__item"
+                        >
+                            <div
+                                className="slider__img"
+                                data-swiper-parallax={item.id % 2 ? "20%" : "30%"}
+                                style={{backgroundImage: `url("${item.img}")`}}
+                            ></div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </>
     );
 };
 
